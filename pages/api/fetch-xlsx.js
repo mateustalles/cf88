7371f11e7377e84a4c5c9ae70d8689d5ef02cd74
Mateus Talles <mateustalles@gmail.com>
2021-05-04 01:03:34 -0300

    feat(fetchXLSX) api

--- pages/api/fetch-xlsx.js
+++ pages/api/fetch-xlsx.js
@@ -1,16 +1,20 @@
 import nc from 'next-connect';
 import { getAllPages } from '@/db/index';
+import { fetchXlsx } from '@/lib/index';
 import { all } from '@/middlewares/index';
 
 const handler = nc();
 handler.use(all);
 
 handler.get(async (req, res) => {
-  await getAllPages(req.db).
-    then((data) => res.status(200).json(data))
+  const allPages = await getAllPages(req.db).
+    then((data) => data)
     .catch((err) => {
       throw Error(err);
     });
+
+  if(fetchXlsx(allPages)) return res.status(200).send('Arquivo criado com sucesso');
+  throw new Error(500, 'Erro na criação de arquivo');
 })
 
 export default handler;
