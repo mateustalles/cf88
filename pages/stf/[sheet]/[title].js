5137d110d8d3adc8ce7ffacb06b051d08591f57a
Mateus Talles <mateustalles@gmail.com>
2021-06-27 23:08:41 -0300

    ft: applying Bootstrap to Thesis pages

--- pages/stf/[sheet]/[title].js
+++ pages/stf/[sheet]/[title].js
@@ -1,10 +1,15 @@
 /* eslint-disable react/prop-types */
-import React, { useRouter } from 'next/router'
+import React, { useRouter, useState } from 'next/router'
 import { findPage, getAllPages } from '../../../models/pagesModel';
 import '../../../styles/Verbatim.module.css'
 import Head from 'next/head';
+import Modal from 'react-bootstrap/Modal';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Verbatim = ({ page }) => {
+  const [showLeadModal, setShowLeadModal] = useState(true);
   const router = useRouter();
 
   if (router.isFallback) {
@@ -19,20 +24,34 @@ const Verbatim = ({ page }) => {
   const pageData = Object.values(data);
   const content = Object.entries(pageData[0])[0][1].replace(/\n/g, ' ');
   return (
-    <div className="verbatim">
+    <Container className="verbatim">
       <Head>
         <title>{`${sheetTitle} - ${pageTitle}`}</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content={content} />
       </Head>
-      <h1>{sheetTitle}: {pageTitle}</h1>
-      { pageData.map((entry, index) => {
-        const [[title, value]] = Object.entries(entry);
-        if (index === 0) return <h3 key="value">{value}</h3>;
-        else if (index === data.length - 1) return;
-        return <p key={`${title}_${value}`}>{title.toUpperCase() + ':'} {value}</p>;
-      }) }
-    </div>
+      <Row>
+        <h1>{sheetTitle}: {pageTitle}</h1>
+      </Row>
+      <Row>
+        <Col>
+          { pageData.map((entry, index) => {
+            const [[title, value]] = Object.entries(entry);
+            if (index === 0) return (
+              <Row>
+                <h3 key="value">{value}</h3>
+              </Row>
+            );
+            else if (index === data.length - 1) return;
+            return (
+              <Row key={`${title}_${value}`}>
+                <p>{title.toUpperCase() + ':'} {value}</p>
+              </Row>
+            )
+          }) }
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
