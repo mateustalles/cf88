213aa8dcad9d94dee8a1c2ca04fcef71ba47aa5c
Mateus Talles <mtalles@avantstay.com>
2021-08-27 01:08:20 -0300

    commented unecessary function

--- pages/api/page/get-10-most-viewed.js
+++ pages/api/page/get-10-most-viewed.js
@@ -1,17 +1,17 @@
-import nc from 'next-connect';
-import { get10MostViewed } from '@/db/index'
-import { all } from '@/middlewares/index'
+// import nc from 'next-connect';
+// import { get10MostViewed } from '@/db/index'
+// import { all } from '@/middlewares/index'
 
-const handler = nc();
-handler.use(all);
+// const handler = nc();
+// handler.use(all);
 
-handler.post(async (req, res) => {
-  await get10MostViewed(req.db)
-    .then((data) => res.send(200).json(data))
-    .catch((err) => {
-      console.log(err.message)
-      throw new Error(err);
-    })
-});
+// handler.post(async (req, res) => {
+//   await get10MostViewed(req.db)
+//     .then((data) => res.send(200).json(data))
+//     .catch((err) => {
+//       console.log(err.message)
+//       throw new Error(err);
+//     })
+// });
 
-export default handler;
+// export default handler;
